ebdd3ebd1b6c040511f2469fdcbc9c759b78f3dc
ozel-byte <ozelcode@gmail.com>
2021-07-06 09:19:44 -0500

    fisrt se

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server,{
+    cors: {
+        methods: ["GET,POST"]
+    }
+});
 const sequelize = require("./DATABASE/db");
-const {Server} = require("socket.io");
 const userRouter = require('./routes/UserRoutes');
-const io = new Server(server);
 var bodyParser = require('body-parser')
 const cors = require('cors');
 let  mapArreglo = new Map();
@@ -17,25 +21,18 @@ app.use(cors());
 io.on('connection', socket => {
 
     console.log("Nueva conexion user"+ socket.id + " "+socket.data);
-
-    mapArreglo.set(socket.id , socket.data);
-
-    socket.on('message', (data) => {
-        console.log("mensaje"+data)
-    });
-
 });
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use('/user',userRouter);
+
 app.get('/', (req,res)=> {
     res.send("hola ")
 });
 
 
-
-app.listen(3001, ()=> {
+server.listen(3001, ()=> {
     console.log("server conectado");
     sequelize.sync({force:false}).then(() => {
         console.log("conexion")
